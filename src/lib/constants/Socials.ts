4cf9a4d59aa13d91be9272d6a924db52de771733
Walledgarden <info@walledgarden.cc>
2023-06-22 20:08:54 +0200

    feat: Add Socials

---
+++ src/lib/constants/Socials.ts
@@ -0,0 +1,36 @@
+import { faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, type IconDefinition } from '@fortawesome/free-solid-svg-icons';
+
+export enum Social {
+	Twitter = 'twitter',
+	Github = 'github',
+	Email = 'email'
+}
+
+export interface SocialData {
+	name: string;
+	url: string;
+	icon: IconDefinition;
+	primaryColor: string;
+}
+
+export const socials: Record<Social, SocialData> = {
+	[Social.Twitter]: {
+		name: 'Twitter',
+		url: 'https://twitter.com/embedbuilder',
+		icon: faTwitter,
+		primaryColor: '#1c9bf0'
+	},
+	[Social.Github]: {
+		name: 'Github',
+		url: 'https://github.com/embedbuilder/website',
+		icon: faGithub,
+		primaryColor: '#e6edf3'
+	},
+	[Social.Email]: {
+		name: 'Email',
+		url: 'mailto:support@embedbuilder.com',
+		icon: faEnvelope,
+		primaryColor: '#ffff00'
+	}
+};
